701720235329cc3923dbb654d501ac6ac67aa6eb
gam0022 <gam0022@gmail.com>
2019-12-15 22:10:51 +0900

    FpsChanger

---
+++ Assets/Experiments/Components/FpsChanger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsChanger : MonoBehaviour
+{
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 1;
+    }
+}
