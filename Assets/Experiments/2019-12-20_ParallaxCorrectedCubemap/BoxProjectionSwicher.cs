de4272fd5fdef4e033d3f62d00106cca02d98391
gam0022 <gam0022@gmail.com>
2019-12-22 13:56:12 +0900

    Import TextMeshPro and Add BoxProjectionSwicher

---
+++ Assets/Experiments/2019-12-20_ParallaxCorrectedCubemap/BoxProjectionSwicher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BoxProjectionSwicher : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textMeshPro;
+    [SerializeField] ReflectionProbe reflectionProbe;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            reflectionProbe.boxProjection = !reflectionProbe.boxProjection;
+        }
+
+        textMeshPro.text = "Box Projection: " + (reflectionProbe.boxProjection ? "ON" : "OFF");
+    }
+}
